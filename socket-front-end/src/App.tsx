1d6f048729e3f3a668fa07354dd9b471380eb529
Octavio Carpes <octavio.carpes10@gmail.com>
2020-03-28 19:36:59 -0300

    Adding projects

--- socket-front-end/src/App.tsx
+++ socket-front-end/src/App.tsx
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, MouseEvent } from 'react';
 import './App.css';
+import style from 'styled-components';
 
-function App() {
+const Container = style.div`
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  height: 80%;
+  border: 1px solid;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 10% 90%;
+
+`;
+
+const Header = style.header`
+
+  position: relative;
+  left: 50%;
+  transform: translate(-50%);
+  width: 80%;
+  height: 100%;
+
+  title {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+      margin: 0;
+      margin-top: 20px;
+    }
+  }
+
+`;
+
+const UploadContainer = style.div`
+
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 100%;
+
+  div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    form {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+
+      button {
+        width: 200px;
+        height: 75px;
+        color: white;
+        background: black;
+        padding: 10px;
+        font-size: 16px;
+        font-weight: bold;
+
+        &:hover {
+          cursor-pointer;
+        }
+      }
+    }
+  }
+`;
+
+const App = () => {
+  const TCP_HOST = 'http://localhost:4000';
+  const UDP_HOST = 'http://localhost:5000';
+  const [tcp, setTcp] = useState('');
+  const [udp, setUdp] = useState('');
+
+  const connectWithTCP = (event: MouseEvent): void => {
+    event.preventDefault();
+    const socket = require('socket.io-client')(TCP_HOST);
+    socket.on('connect', () => {
+      setTcp(`TCP Connected, Uploading file with TCP server.`);
+      const TcpFile: HTMLElement = document.getElementById('tcp-input')!;
+      TcpFile.click();
+      TcpFile.onchange = (file: any) => {
+        console.log(file.target.value)
+        socket.emit('Upload', { 'Name': 'File', 'Data': file.target.value})
+      };
+    });
+  };
+
+  const connectWithUDP = (event: MouseEvent): void => {
+    event.preventDefault();
+    const socket = require('socket.io-client')(UDP_HOST);
+    socket.on('connect', () => {
+      setUdp('UDP Connected, Uploading file with UDP server.')
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header>
+          <title>
+            <h1>Tarefa 2 - Laboratorio de redes</h1>
+          </title>
+        </Header>
+        <UploadContainer>
+            <div>
+              <form>
+                <p>{tcp}</p>
+                <button onClick={(event: MouseEvent) => connectWithTCP(event)}>Upload com TCP</button>
+                <input type='file' id='tcp-input'/>
+                <button>Submit</button>
+              </form>
+            </div>
+            <div>
+              <form>
+                <p>{udp}</p>
+                <input type='file' id='udp-input'/>
+                <button onClick={(event: MouseEvent) => connectWithUDP(event)}>Upload com UDP</button>
+                <button>Submit</button>
+              </form>
+            </div>
+        </UploadContainer>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
